01aa1685ce286914c5e6e22a57a6dbdc23b87605
Rick Donato <rick.donato@packetcoders.io>
2023-09-02 08:36:55 +0100

    Bootstrapping

---
+++ src/helpers.py
@@ -0,0 +1,13 @@
+import json
+import yaml
+
+# Load YAML or JSON
+def load_yaml_or_json(filename):
+    if filename.endswith(".yaml") or filename.endswith(".yml"):
+        with open(filename, "r") as f:
+            return yaml.safe_load(f)
+    elif filename.endswith(".json"):
+        with open(filename, "r") as f:
+            return json.load(f)
+    else:
+        raise Exception(f"Unknown file type for {filename}")
