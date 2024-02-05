6ae43dec1c58fd9279540d33b13df912cd03bacc
Rick Donato <rick.donato@packetcoders.io>
2024-02-05 17:15:30 +0000

    Add cli

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
+#! /usr/bin/env python
+
 import pathlib
 import sys
 from pathlib import Path
 
-from rich import print as rprint
-
 sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute()))
+
+import click
+from rich import box
+from rich.console import Console
+from rich.table import Table
 
 from src.helpers import load_yaml_or_json
 from src.plugins.json_schema.validator import JSONSchemaValidator
+from helpers import read_yaml
+
 
 
 class SchemaValidator:
@@ -38,6 +45,7 @@ class SchemaValidator:
     def __init__(self, document_path, schema, validator_engine):
         self._validator = validator_engine
         self._document_path = Path(document_path)
+        print(schema)
         self._schema = Path(schema)
         self._errors = []
         self._warnings = []
@@ -81,23 +89,68 @@ class SchemaValidator:
         return self._validate()
 
 
-def run():
+@click.command()
+@click.option(
+    "--document_path",
+    "-p",
+    type=click.Path(),
+    required=True,
+    help="path",
+)
+@click.option(
+    "--schema",
+    "-s",
+    type=click.Path(),
+    required=True,
+    help="schema",
+    default=f"{Path(__file__).parent}/schema.yml",
+)
+def main(document_path, schema):
     """
-    Runs the schema validation process.
+    It iterates through the files in the directory and validates them.
 
-    This function initializes a SchemaValidator object, sets the document path,
-    schema, and validator engine, and then runs the validation process. Finally,
-    it prints the validation results.
-
+    :param document_path: The path to the directory containing the YAML files to be validated
+    :param schema: The path to the schema file
     """
+    print(schema)
+    schema = read_yaml(schema)
+
+    console = Console()
+
+    # Define the table
+    table = Table(show_header=True, header_style="bold magenta", box=box.HORIZONTALS)
+    table.add_column("Result")
+    table.add_column("Msg")
+    table.add_column("Value")
+    table.add_column("Filename")
+
     schema_validator = SchemaValidator(
-        document_path="examples/host_vars",
-        schema="examples/schema.yaml",
+        document_path=document_path,
+        schema=schema,
         validator_engine=JSONSchemaValidator(),
     )
     schema_validator.initialize()
-    rprint(schema_validator.results)
+
+    errors = False
+
+    for error in schema_validator.results:
+        if error["error"]:
+            errors = True
+            table.add_row(
+                ":cross_mark:", error["msg"], error["value"], error["filename"]
+            )
+        else:
+            table.add_row(
+                ":white_heavy_check_mark:", "No errors", None, error["filename"]
+            )
+
+    # Display the table
+    console.print(table)
+
+    # If the validation result is false, then the program will exit with a status code of 1.
+    if errors:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    run()
+    main()
